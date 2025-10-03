8f2fa3e0abcc0089108718eec1a23ee4b45d92ce
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-03 15:44:10 +0530

    updated

---
+++ C2TC_CoreJava8/src/day2/LoopDemo.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class LoopDemo {
+	
+	    public static void main(String[] args) {
+	        
+	        // Loop from 1 to 10
+	        for (int i = 1; i <= 10; i++) {
+	            
+	            // Check if number is even or odd
+	            if (i % 2 == 0) {
+	                System.out.println(i + " is Even");
+	            } else {
+	                System.out.println(i + " is Odd");
+	            }
+	        }
+	    }
+	}
+
+
