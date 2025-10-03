7f55ef4379b5e7c45a4de178e5961218ed549be8
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-03 15:36:25 +0530

    updated

---
+++ C2TC_CoreJava8/src/day1/Identifiers.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Identifiers {
+	public static void main(String[] args) {
+		
+				
+				int $number = 20; //starts with $
+				System.out.println("value of the number variable is : "+ $number);
+			
+				String studentName="shanuu"; //use camelcase
+				System.out.println("value is : "+ studentName);
+
+	}
+
+}
