23ea11fe4f08aa67670bbc321af3507b66cb65c7
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-03 16:25:24 +0530

    updated

--- C2TC_CoreJava8/src/day3/encapsulation/Encapsulation.java
+++ C2TC_CoreJava8/src/day3/encapsulation/Encapsulation.java
@@ -2,8 +2,16 @@ package day3.encapsulation;
 
 public class Encapsulation {
 
-	public Encapsulation() {
-		// TODO Auto-generated constructor stub
-	}
+	
+		public static void main(String[] args) {
+			
+			//object creation 
+			OopsConceptDemo obj = new OopsConceptDemo();
+			obj.setSerialNum(101);
+			obj.setName("Dharshini");
+			obj.setAge(20);
+			System.out.println(obj);
+			
+		}
 
 }
