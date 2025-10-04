aeb1757af4d3a1b2d0bcdb70c69d7fc2090c9eab
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-04 12:11:52 +0530

    assignment

---
+++ C2TC_CoreJava8/src/com/tnsif/assigmentone/MainClass.java
@@ -0,0 +1,10 @@
+package com.tnsif.assigmentone;
+
+public class MainClass {
+	   public static void main(String[] args) {
+	        Student s1 = new Student();
+	        Commission emp = new Commission();
+	        emp.acceptDetails();
+	        emp.calculateCommission();
+	    }
+}
---
+++ C2TC_CoreJava8/src/com/tnsif/assigmentone/Student.java
@@ -0,0 +1,8 @@
+package com.tnsif.assigmentone;
+
+public class Student {
+	
+	    public Student() {
+	        System.out.println("Student object is created");
+	    }
+}
