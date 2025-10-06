5a9c0b0d5422f6793afd88e5926a175010d75429
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-06 21:34:59 +0530

    updated day4

--- C2TC_CoreJava8/src/com/tnsif/dayfour/thirdpackage/PrivateConstructor.java
+++ C2TC_CoreJava8/src/com/tnsif/dayfour/thirdpackage/PrivateConstructor.java
@@ -1,5 +1,17 @@
 package com.tnsif.dayfour.thirdpackage;
 
 public class PrivateConstructor {
-
+	public static void main(String s[]) {
+		// Can't create object with private constructor
+		// MyClass m1=new MyClass();
+		
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
 }
