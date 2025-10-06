2ef618a9a92373894a256fd5f36ca948572cae21
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-06 21:28:29 +0530

    updated day4

---
+++ C2TC_CoreJava8/src/com/tnsif/dayfour/secondpackage/Executor.java
@@ -0,0 +1,13 @@
+package com.tnsif.dayfour.secondpackage;
+import com.tnsif.dayfour.firstpackage.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+		Base b1 = new Base();
+
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+
+	}
+}
