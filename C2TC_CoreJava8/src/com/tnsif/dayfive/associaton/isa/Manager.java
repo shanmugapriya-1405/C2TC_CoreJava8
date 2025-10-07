40f1a3b635a1d0f66a16a682365598fdacc56f01
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-07 20:31:22 +0530

    updated

---
+++ C2TC_CoreJava8/src/com/tnsif/dayfive/associaton/isa/Manager.java
@@ -0,0 +1,29 @@
+package com.tnsif.dayfive.associaton.isa;
+
+
+	public class Manager extends Employee {
+		private int teamSize;
+
+		public Manager() {
+			super();
+
+		}
+
+		public Manager(String name, int employeeId, String department, int teamSize) {
+			super(name, employeeId, department);
+			this.teamSize = teamSize;
+		}
+
+		public int getTeamSize() {
+			return teamSize;
+		}
+
+		public void setTeamSize(int teamSize) {
+			this.teamSize = teamSize;
+		}
+
+		public String toString() {
+			return "Manager [TeamSize=" + teamSize + ", Name=" + getName() + ", EmployeeId=" + getEmployeeId()
+					+ ", Department=" + getDepartment() + "]";
+		}
+}
