d0d9bdb534d64feeff252c5d9608bdacff90ef9e
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-08 21:54:28 +0530

    updated

--- C2TC_CoreJava8/src/com/tnsif/daysix/usingfinal/FinalMethodClass.java
+++ C2TC_CoreJava8/src/com/tnsif/daysix/usingfinal/FinalMethodClass.java
@@ -1,5 +1,15 @@
 package com.tnsif.daysix.usingfinal;
 
 public class FinalMethodClass {
+	public class FinalMethodClass {
+		FinalMethodClass() {
+			System.out.println("This is a default constructor");
+		}
 
+		final int a = 50;
+
+		// Final method
+		final void show() {
+			System.out.println("Value of a: " + a);
+		}
 }
