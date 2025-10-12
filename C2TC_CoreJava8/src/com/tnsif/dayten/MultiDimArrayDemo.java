cfc06402e3651553c051b16ad8e6434819198917
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-12 12:38:10 +0530

    updated

--- C2TC_CoreJava8/src/com/tnsif/dayten/MultiDimArrayDemo.java
+++ C2TC_CoreJava8/src/com/tnsif/dayten/MultiDimArrayDemo.java
@@ -1,5 +1,13 @@
 package com.tnsif.dayten;
 
 public class MultiDimArrayDemo {
+	public static void main(String[] args) {
+
+		int c[][] = { { 12, 45 }, { 10, 40 }, { 5, 7 } }; // new int[3][2];
+
+		System.out.println("No. of Rows in C array : " + c.length);
+
+		MLArray.printArray(c);
+	}
 
 }
