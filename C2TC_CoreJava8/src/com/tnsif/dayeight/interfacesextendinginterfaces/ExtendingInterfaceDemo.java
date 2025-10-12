d5aaecab581b578a3f02fbe139b0a0b176409709
Shanmuga05 <priyasupramani1405@gmail.com>
2025-10-12 11:12:51 +0530

    update

---
+++ C2TC_CoreJava8/src/com/tnsif/dayeight/interfacesextendinginterfaces/ExtendingInterfaceDemo.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayeight.interfacesextendinginterfaces;
+
+public class ExtendingInterfaceDemo {
+
+}
---
+++ C2TC_CoreJava8/src/com/tnsif/dayeight/interfacesextendinginterfaces/InterfaceOne.java
@@ -0,0 +1,5 @@
+package com.tnsif.dayeight.interfacesextendinginterfaces;
+
+interface InterfaceOne{  
+	  void print();  
+	} 
